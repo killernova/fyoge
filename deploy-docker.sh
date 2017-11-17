d3ef42cfa07fb232a2796d282483e2fc619af67b
killernova <ye_sijie0101@wuxiapptec.com>
2017-11-17 23:30:36 +0800

    remove sensitive file

--- deploy-docker.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-ssh root@101.132.158.123 <<'ENDSSH'
-cd ~/fyoga
-git pull origin master
-cp -f fyoga-production /etc/logrotate.d/fyoga-production
-docker-compose up --build
-exit
-ENDSSH
-
