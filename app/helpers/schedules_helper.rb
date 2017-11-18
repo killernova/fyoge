426f692a5b4b1c2c743b2adf23f180270a363f5e
killernova <ye_sijie0101@wuxiapptec.com>
2017-11-18 19:47:11 +0800

    bug fix

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,9 +1,9 @@
 module SchedulesHelper
 
-  def find_schedule(schedules, key, index)
+  def find_schedule(schedules, weekday, serial_number)
     return nil if schedules.nil?
-    schedules = schedules.where(weekday: key)
+    schedules = schedules.where(weekday: weekday)
     return nil if schedules.blank?
-    schedules.find { |s| s.serial_number == index }
+    schedules.find { |s| s.serial_number == serial_number }
   end
 end
